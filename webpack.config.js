6e5d6c7db20601e263a03181faca598b7bdee169
Vien Loi <vvloi92@gmail.com>
2019-03-28 23:51:47 +0700

    complete code as tutorial of webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: "./src/index.js",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: '/'
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    module: {
+        rules: [
+        {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ["file-loader"]
+        }
+        ]
+    }
+};
